ceb92a0f9758d6af20106f855a502e6a7e321cb9
Shreya <ghoshshreya932@gmail.com>
2025-11-09 22:56:20 +0530

    REversed changes

--- Backend/server.js
+++ Backend/server.js
@@ -9,11 +9,7 @@ import Patient from './src/models/Patient.js';
 
 const app = express();
 
-app.use(cors({
-  origin: "https://aurafrontend-p577yi3cs-shreyas-projects-f842d25f.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
@@ -23,18 +19,7 @@ app.use('/api/user', userRoutes);
 connectDB();
 
 // CRON job runs every midnight
-cron.schedule("0 0 * * *", async () => {
-  try {
-    console.log("⏳ Resetting medication status at 12 AM...");
-    await Patient.updateMany(
-      {},
-      { $set: { "diseases.$[].medications.$[].timing.$[].status": "pending" } }
-    );
-    console.log("✅ Medication reset done.");
-  } catch (error) {
-    console.error("❌ Error resetting medication:", error);
-  }
-});
+
 
 
 export default app;
