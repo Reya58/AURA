2912dcab7e6c07f7157561b7b688c143f6c3d3f5
Shreya <ghoshshreya932@gmail.com>
2025-11-09 22:29:55 +0530

    Backend Updated

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import connectDB from './src/config/db.js';
 import authRoutes from './src/routes/auth.routes.js';
 import userRoutes from './src/routes/user.routes.js';
+import cron from "node-cron";
+import Patient from './src/models/Patient.js';
 
 const app = express();
 app.use(cors());
@@ -14,5 +16,30 @@ app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 connectDB();
+
+
+
+
+// Run every day at 12:00 AM
+cron.schedule("0 0 * * *", async () => {
+  try {
+    console.log("⏳ Resetting medication status at 12 AM...");
+
+    // Update every patient's medication timing slots to pending
+    await Patient.updateMany(
+      {},
+      {
+        $set: {
+          "diseases.$[].medications.$[].timing.$[].status": "pending"
+        }
+      }
+    );
+
+    console.log("✅ All medication statuses reset to 'pending'.");
+  } catch (error) {
+    console.error("❌ Error resetting medication:", error);
+  }
+});
+
 
 app.listen(PORT, () => console.log(`🔥 Server running on port ${PORT}`));
