3b76d0c0648829e7113a08caf3c1bdf3c785cf53
Shreya <ghoshshreya932@gmail.com>
2025-11-09 23:19:13 +0530

    Donest

--- Backend/server.js
+++ Backend/server.js
@@ -17,12 +17,14 @@ app.use(cors({
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Aura Health App API');
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
-connectDB();
-
 connectDB();
 
 app.listen(PORT, () => {
