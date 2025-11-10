16f3f788340e7df555f4c816236c825ceaf4958f
Shreya <ghoshshreya932@gmail.com>
2025-11-10 11:40:55 +0530

    Testing it

--- Backend/server.js
+++ Backend/server.js
@@ -22,18 +22,7 @@ app.get('/', (req, res) => {
 });
 
 // Inside server.js
-app.get("/api/cron/run", async (req, res) => {
-  try {
-    // Your cron logic
-    await Patient.updateMany(
-      {},
-      { $set: { "diseases.$[].medications.$[].timing.$[].status": "pending" } }
-    );
-    return res.status(200).json({ message: "Cron triggered!" });
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-});
+
 
 
 app.use('/api/auth', authRoutes);
